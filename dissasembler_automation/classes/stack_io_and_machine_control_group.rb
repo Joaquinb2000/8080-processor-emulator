ae2f71bf482d318cb4911c1ac4d7deca28af3d46
Joaquinb2000 <joaquinberon2000@gmail.com>
2024-05-05 22:04:35 -0400

    Add Stack, IO, and Machine Control group operations

---
+++ dissasembler_automation/classes/stack_io_and_machine_control_group.rb
@@ -0,0 +1,44 @@
+require_relative 'processor_8080_registers'
+
+class StackIoAndMachineControlGroup < Processor8080Registers
+  PUSH = REGISTER_PAIRS.map { |register_pair, binary| new("PUSH #{register_pair}", "11#{binary}0101") }
+
+  PUSH_PROCESSOR_STATUS_WORD = new('PUSH PSW', '11110101')
+
+  POP = REGISTER_PAIRS.map { |register_pair, binary| new("POP #{register_pair}", "11#{binary}0001") }
+
+  POP_PROCESSOR_STATUS_WORD = new('POP PSW', '11110001')
+
+  EXCH_STACK_TOP_WITH_H_AND_L = new('XTHL', '11100011')
+
+  MOVE_HL_TO_SP = new('SPHL', '11111001')
+
+  INPUT = new('IN port', '11011011')
+
+  OUTPUT = new('OUT port', '11010011')
+
+  ENABLE_INTERRUPTS = new('EI', '11111011')
+
+  DISABLE_INTERRUPTS = new('DI', '11110011')
+
+  HALT = new('HLT', '01110110')
+
+  NO_OP = new('NOP', '00000000')
+
+  def self.operations
+    [
+      *PUSH,
+      *POP,
+      PUSH_PROCESSOR_STATUS_WORD,
+      POP_PROCESSOR_STATUS_WORD,
+      EXCH_STACK_TOP_WITH_H_AND_L,
+      MOVE_HL_TO_SP,
+      INPUT,
+      OUTPUT,
+      ENABLE_INTERRUPTS,
+      DISABLE_INTERRUPTS,
+      HALT,
+      NO_OP
+    ]
+  end
+end
--- dissasembler_automation/processor_8080_operations_as_cpp_cases.rb
+++ dissasembler_automation/processor_8080_operations_as_cpp_cases.rb
@@ -2,12 +2,14 @@ require './classes/data_transfer_group'
 require './classes/arithmetic_group'
 require './classes/logical_group'
 require './classes/branch_group'
+require './classes/stack_io_and_machine_control_group'
 
 operations = [
   *DataTransferGroup.operations,
   *ArithmeticGroup.operations,
   *LogicalGroup.operations,
-  *BranchGroup.operations
+  *BranchGroup.operations,
+  *StackIoAndMachineControlGroup.operations
 ]
 
 operations.sort { |op1, op2| op1.hex <=> op2.hex }.each do |operation|
